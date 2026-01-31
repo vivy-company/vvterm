8b2cb4ea9b5a0292c45f1622547df3954de658ff
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-01-31 19:27:34 +0800

    Bump version to 1.1

--- VVTerm/Views/Settings/AboutSettingsView.swift
+++ VVTerm/Views/Settings/AboutSettingsView.swift
@@ -36,7 +36,7 @@ struct AboutSettingsView: View {
     @State private var showingReviewSheet = false
 
     private var appVersion: String {
-        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.1"
     }
 
     private var buildNumber: String {
